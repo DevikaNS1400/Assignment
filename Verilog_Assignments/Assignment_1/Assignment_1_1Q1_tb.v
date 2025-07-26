dcd7ba677305f625bd19a1a40b1fae96d16d9f48
N S Devika <nsdevika2000@gmail.com>
2025-07-26 10:23:47 +0530

    Add files via upload

---
+++ Verilog_Assignments/Assignment_1/Assignment_1_1Q1_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.07.2025 10:00:56
+// Design Name: 
+// Module Name: Assignment_1_1Q1_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module netwire_tb;
+netwire u1();
+initial
+begin
+#20;
+$finish;
+end
+endmodule
