b534d430c7331d5e473871481f5b471063a34669
N S Devika <nsdevika2000@gmail.com>
2025-09-09 13:17:25 +0530

    Add files via upload

---
+++ Verilog_Assignments/Task_Functions_2/Assignment_tf_Q50.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.09.2025 13:09:50
+// Design Name: 
+// Module Name: mod_write_display
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mod_write_display();
+reg [2:0]a,b;
+initial begin
+a=3'b010;b=3'b101;
+$display("Using $dispaly");
+$display("a=%b",a);
+$display("b=%b",b);
+$display("\nUsing $write");
+$write("a=%b",a);
+$write("b=%b",b);
+$finish;
+end
+endmodule
