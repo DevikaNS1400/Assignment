f7a3e99cdb9b60a75ca9999891d29a0eedaf14c4
N S Devika <nsdevika2000@gmail.com>
2025-09-10 09:53:27 +0530

    Add files via upload

---
+++ Verilog_Assignments/Task_Functions_2/Assignment_tf_Q51.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.09.2025 14:31:02
+// Design Name: 
+// Module Name: Assignment_tf_Q51
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module not_gate(
+output y,
+input x
+    );
+assign y=~x;    
+endmodule
